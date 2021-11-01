75fc52145a1445a957aa1f57f18bea41a4a20e43
jxxghp <jxxghp@163.com>
2021-11-01 13:40:06 +0800

    bug fix

--- log.py
+++ log.py
@@ -1,8 +1,8 @@
 import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
-
 import settings
+from functions import mysql_exec_sql
 
 
 class Logger:
@@ -17,5 +17,22 @@ class Logger:
         log_file_handler = TimedRotatingFileHandler(filename=logpath + "/" + logname + ".txt", when="D", interval=1, backupCount=2)
         log_file_handler.setFormatter(formatter)
         log_console_handler = logging.StreamHandler()
+        mysql_filter = LoggerToMysqlFilter()
         self.logger.addHandler(log_file_handler)
         self.logger.addHandler(log_console_handler)
+        self.logger.addFilter(mysql_filter)
+
+
+class LoggerToMysqlFilter(logging.Filter):
+    def filter(self, record):
+        rtext = record.msg
+        rname = record.name
+        rtype = record.levelname
+        if rtype == "INFO" or rtype == "ERROR":
+            sql = "INSERT INTO system_log \
+                                    (TYPE, NAME, TEXT, TIME) \
+                                    VALUES ('%s', '%s', '%s', now())" % \
+                  (rtype, rname, rtext)
+            # 登记数据库
+            mysql_exec_sql(sql)
+        return True
