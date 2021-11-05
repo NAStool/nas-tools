58994ff5af1e34a4413e8d6753137c0810ce0f24
Mr Huang <51039935+jxxghp@users.noreply.github.com>
2021-11-05 11:34:04 +0800

    Add files via upload

--- web/static/js/util.js
+++ web/static/js/util.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
-    //初始化编辑器
+    // 初始化编辑器
     var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/github");
 	editor.session.setMode("ace/mode/ini");
 	editor.setFontSize(14);
 
+    // Ajax主方法
+    function ajax_post(cmd, data, handler){
+        var data = {
+	        cmd: cmd,
+	        data: JSON.stringify(data)
+	    };
+        $.ajax({
+            type: "POST",
+            url: "do",
+            dataType: "json",
+            data: data,
+            success: handler,
+            error: function(xhr, textStatus, errorThrown){
+                alert("错误信息:"+xhr.responseText);
+            }
+        });
+    }
+
+    // qBittorrent下拉选择变化
 	$("#rmt_path").change(function(){
 	    path = $("#rmt_path").val();
 	    pos = path.lastIndexOf("/")
@@ -12,6 +31,7 @@ $(document).ready(function(){
 	    $("#rmt_name").val(name);
 	});
 
+    // 触发定时服务按钮
 	$(".sch_action_btn").click(function(){
 	    var cmd = "sch";
 	    var data = {
@@ -27,6 +47,21 @@ $(document).ready(function(){
 	        $("#" + item).removeAttr("disabled");
 	        $("#" + item).text("启动");
 	    })
+	});
+
+    // 获取qBittorrent下拉框内容
+    $("#rmt_tab").click(function(){
+	    var cmd = "rmt_qry";
+	    var data = {};
+	    ajax_post(cmd, data, function(ret){
+	        rmt_paths = ret.rmt_paths;
+            $("#rmt_path").empty();
+            $("#rmt_path").append("<option value =\"\">全部</option>");
+            for(var i=0; i<rmt_paths.length; i++){
+                path = rmt_paths[i].split("|")[0];
+                $("#rmt_path").append("<option value=\"" + rmt_paths[i] + "\">" + path + "</option>");
+            }
+	    });
 	});
 
     // qBittorrent转移按钮
@@ -81,7 +116,17 @@ $(document).ready(function(){
 	    });
 	});
 
-    // 配置文件按钮
+	// 获取配置文件
+    $("#set_tab").click(function(){
+        var cmd = "set_qry";
+        var data = {}
+        ajax_post(cmd, data, function(ret){
+	        config_str = ret.config_str;
+	        editor.getSession().setValue(config_str)
+	    });
+    });
+
+    // 配置文件保存按钮
 	$("#set_btn").click(function(){
 	    var cmd = "set";
 	    var data = {
@@ -90,29 +135,29 @@ $(document).ready(function(){
 	    $("#set_btn").text("正在处理...");
 	    $("#set_btn").attr("disabled", "true");
 	    ajax_post(cmd, data, function(ret){
-	        config_str = ret.config_str;
-	        editor.getSession().setValue(config_str)
 	        $("#set_btn").removeAttr("disabled");
 	        $("#set_btn").text("保存");
 	    });
 	});
 
-    // Ajax主方法
-    function ajax_post(cmd, data, handler){
-        var data = {
-	        cmd: cmd,
-	        data: JSON.stringify(data)
-	    };
-        $.ajax({
-            type: "POST",
-            url: "do",
-            dataType: "json",
-            data: data,
-            success: handler,
-            error: function(xhr, textStatus, errorThrown){
-                alert("错误信息:"+xhr.responseText);
+	// 显示日志
+    $("#log_tab").click(function(){
+	    var cmd = "log_qry";
+	    var data = {};
+	    ajax_post(cmd, data, function(ret){
+	        log_list = ret.log_list;
+	        $("#log_table").empty();
+	        $("#log_table").append("<caption>日志</caption><thead><tr><th>序号</th><th>级别</th><th>名称</th><th>内容</th><th>时间</th></tr></thead>");
+            for(log_item in log_list){
+                $("#log_table").append("<tbody><tr>");
+                $("#log_table").append("<td>" + log_item[0] + "</td>");
+                $("#log_table").append("<td>" + log_item[1] + "</td>");
+                $("#log_table").append("<td>" + log_item[2] + "</td>");
+                $("#log_table").append("<td>" + log_item[3] + "</td>");
+                $("#log_table").append("<td>" + log_item[4] + "</td>");
+                $("#log_table").append("</tr><tbody>");
             }
-        });
-    }
+	    });
+	});
 
 });
