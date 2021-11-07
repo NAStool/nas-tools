1cdb9b02b48b254b4a6c26c4ffbc27e75342f37e
jxxghp <jxxghp@163.com>
2021-11-07 08:18:59 +0800

    bug fix

--- scheduler/rss_download.py
+++ scheduler/rss_download.py
@@ -18,6 +18,7 @@ rss_jobs = eval(settings.get("rss.rss_job"))
 save_path = settings.get("rss.save_path")
 movie_path = settings.get("rss.movie_path")
 tv_path = settings.get("rss.tv_path")
+media_exts = settings.get("rmt.rmt_mediaext").split(",")
 rss_cache_list = []
 rss_cache_name = []
 
@@ -130,7 +131,7 @@ def run_rssdownload():
                         continue
                 else:
                     # 剧集目录
-                    media_path = os.path.join(settings.get('rmt.rmt_tvpath'), media_type, media_name)
+                    media_path = os.path.join(tv_path, media_type, media_name)
                     # 剧集是否存在
                     # Sxx
                     file_season = get_media_file_season(title)
@@ -143,10 +144,9 @@ def run_rssdownload():
                         # 集 xx
                         file_seq_num = str(int(file_seq.replace("E", "").replace("P", "")))
                         # 文件路径
-                        file_path = os.path.join(season_dir,
-                                                 media_title + " - " + file_season + file_seq + " - " + "第 " + file_seq_num + " 集")
+                        file_path = os.path.join(season_dir, media_title + " - " + file_season + file_seq + " - " + "第 " + file_seq_num + " 集")
                         exist_flag = False
-                        for ext in settings.get("rmt.rmt_mediaext").split(","):
+                        for ext in media_exts:
                             logger.debug("【RSS】路径：" + file_path + ext)
                             if os.path.exists(file_path + ext):
                                 exist_flag = True
