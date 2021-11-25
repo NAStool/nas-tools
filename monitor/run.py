44bb72c3dfab06809838aaf5f45f5a71d39c693e
Mr Huang <51039935+jxxghp@users.noreply.github.com>
2021-11-25 15:55:04 +0800

    Add files via upload

--- monitor/run.py
+++ monitor/run.py
@@ -1,23 +1,20 @@
 import log
+import settings
 from message.send import sendmsg
 from monitor.movie_trailer import run_movie_trailer
 from monitor.resiliosync import run_resilosync
 
-logger = log.Logger("run").logger
 
-
-def run_monitor():
+def run_monitor(config_file):
+    settings.config_file_path = config_file
+    settings.reload_config()
     try:
         run_movie_trailer()
     except Exception as err:
-        logger.error("【RUN】启动movie_trailer监控失败：" + str(err))
+        log.error("【RUN】启动movie_trailer监控失败：" + str(err))
         sendmsg("【NASTOOL】启动movie_trailer监控失败！", str(err))
     try:
         run_resilosync()
     except Exception as err:
-        logger.error("【RUN】启动resilosync监控失败：" + str(err))
+        log.error("【RUN】启动resilosync监控失败：" + str(err))
         sendmsg("【NASTOOL】启动resilosync监控失败！", str(err))
-
-
-if __name__ == "__main__":
-    run_monitor()
