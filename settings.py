284d41cc1e6a8d255966921d86b18ff0de917364
jxxghp <jxxghp@163.com>
2022-02-16 09:31:27 +0800

    支持硬链接模式转移文件

--- settings.py
+++ settings.py
@@ -36,7 +36,7 @@ class Config(object):
             return default
         option = '.'.join(map_key[1:])
         try:
-            return self.config.get(section, option)
+            return self.config.get(section, option).strip()
         except NoOptionError:
             return default
 
