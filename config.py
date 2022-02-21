5559fd95ba9af576246a47caf8d00c7ae8c713e8
jxxghp <jxxghp@163.com>
2022-02-21 19:08:40 +0800

    支持Transmission做为PT下载器

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import log
 # 菜单对应关系，配置WeChat应用中配置的菜单ID与执行命令的对应关系，需要手工修改
 # 菜单序号在https://work.weixin.qq.com/wework_admin/frame#apps 应用自定义菜单中维护，然后看日志输出的菜单序号是啥（按顺利能猜到的）....
 # 命令对应关系：/qbt qBittorrent转移；/qbr qBittorrent删种；/hotm 热门预告；/pts PT签到；/mrt 预告片下载；/rst ResilioSync同步；/rss RSS下载
-WECHAT_MENU = {'_0_0': '/qbt', '_0_1': '/qbr', '_0_2': '/rss', '_0_3': '/hotm', '_0_4': '/mrt', '_1_0': '/rst',
+WECHAT_MENU = {'_0_0': '/ptt', '_0_1': '/ptr', '_0_2': '/rss', '_0_3': '/hotm', '_0_4': '/mrt', '_1_0': '/rst',
                '_2_0': '/pts'}
 # 电影类型，目前不能修改
 RMT_MOVIETYPE = ['华语电影', '外语电影', '精选']
