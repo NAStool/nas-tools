3641a83400916c9e104186234cd3c4880b46bdca
田志鹏 <zhipeng.tian@biomind.ai>
2022-04-17 17:45:19 +0800

    1.docker 添加supervisor 管理启动，2.顺带尝试解决更新安装依赖的编译问题，3.添加depth加快更新速度

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,13 +7,14 @@ if [ -n $NASTOOL_AUTO_UPDATE ]; then
     fi
     echo "更新程序..."
     git remote set-url origin ${REPO_URL} &>/dev/null
-    git pull
+    git pull --depth=1
     if [ $? -eq 0 ]; then
         echo "更新成功..."
         hash_old=$(cat /tmp/requirements.txt.sha256sum)
         hash_new=$(sha256sum requirements.txt)
         if [ "$hash_old" != "$hash_new" ]; then
             echo "检测到requirements.txt有变化，重新安装依赖..."
+            pip install --upgrade pip setuptools wheel
             pip install -r requirements.txt
             if [ $? -ne 0 ]; then
                 echo "无法安装依赖，请更新镜像..."
@@ -32,4 +33,4 @@ fi
 echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序..."
 chown -R ${PUID}:${PGID} /config /nas-tools
 umask $UMASK
-exec su-exec ${PUID}:${PGID} python3 run.py
+exec su-exec ${PUID}:${PGID} /usr/bin/supervisord -n -c /nas-tools/supervisord.conf
