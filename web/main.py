c41a95435bbf3fdee5d4442dcb3464ebb42c2e91
jxxghp <jxxghp@163.com>
2022-04-24 19:29:56 +0800

    no cache

--- web/main.py
+++ web/main.py
@@ -66,14 +66,9 @@ def create_flask_app(config):
 
     @App.after_request
     def add_header(r):
-        """
-        Add headers to both force latest IE rendering engine or Chrome Frame,
-        and also to cache the rendered page for 10 minutes.
-        """
-        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        r.headers["Cache-Control"] = "no-cache, no-store, max-age=0"
         r.headers["Pragma"] = "no-cache"
         r.headers["Expires"] = "0"
-        r.headers['Cache-Control'] = 'public, max-age=0'
         return r
 
     # 根据用户名获得用户记录
