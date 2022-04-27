adbdf98c0bd1843998e4e4a22b70810086004f57
Mr Huang <51039935+jxxghp@users.noreply.github.com>
2022-04-27 18:30:46 +0800

    v0.6.03

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -31,7 +31,7 @@ else
 fi
 
 echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序..."
-echo "日志将停止打印，请通过文件或WEB页面查看日志"
+echo "注意：日志将停止打印，请通过文件或WEB页面查看日志"
 chown -R ${PUID}:${PGID} /config ${WORKDIR} /var/log/supervisor/
 umask ${UMASK}
 exec su-exec ${PUID}:${PGID} /usr/bin/supervisord -n -c ${WORKDIR}/supervisord.conf
