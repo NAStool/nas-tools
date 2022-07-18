a1f757696faf24311056a5dc1c73926ab9661415
jxxghp <jxxghp@163.com>
2022-07-19 07:05:10 +0800

    修复英文名识别Bug #523

--- rmt/meta/metavideo.py
+++ rmt/meta/metavideo.py
@@ -22,7 +22,7 @@ class MetaVideo(MetaBase):
     _season_re = r"S(\d{2})|^S(\d{1,2})"
     _episode_re = r"EP?(\d{2,4})|^EP?(\d{1,4})"
     _part_re = r"(^PART[0-9]{0,2}$|^CD[0-9]{0,2}$|^DVD[0-9]{0,2}$|^DISK[0-9]{0,2}$|^DISC[0-9]{0,2}$)"
-    _roman_numerals = r"(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})"
+    _roman_numerals = r"^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$"
     _resources_type_re = r"^BLURAY$|^REMUX$|^HDTV$|^UHDTV$|^HDDVD$|^WEBRIP$|^DVDRIP$|^BDRIP$|^UHD$|^SDR$|^HDR\d*$|^DOLBY$|^BLU$|^WEB$|^BD$"
     _name_no_begin_re = r"^\[.+?]"
     _name_se_words = ['共', '第', '季', '集', '话', '話']
@@ -184,15 +184,14 @@ class MetaVideo(MetaBase):
         if not self.get_name():
             return
         re_res = re.search(r"%s" % self._part_re, token, re.IGNORECASE)
-        next = self.tokens.cur()
+        nextv = self.tokens.cur()
         if re_res and\
-            next.isdigit() and (len(next) == 1 or len(next) == 2 and next.startswith('0')):
+            nextv.isdigit() \
+                and (len(nextv) == 1 or len(nextv) == 2 and nextv.startswith('0')):
             self.tokens.get_next()
-
             if not self.part:
                 self.part = re_res.group(1)
-
-            self.part = "%s%s" % (self.part, next)
+            self.part = "%s%s" % (self.part, nextv)
             self._last_token_type = "part"
             self._continue_flag = False
             self._stop_name_flag = False
