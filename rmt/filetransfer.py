7c9fe80c52fc4d0902fa8dd23c57c21f656561a9
JohnZp <455482064@qq.com>
2022-08-02 17:20:08 +0800

    feature: 适配极空间硬连接后极影视不能识别问题

--- rmt/filetransfer.py
+++ rmt/filetransfer.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import platform
 import re
 import traceback
 from enum import Enum
@@ -182,7 +183,12 @@ class FileTransfer:
                     retcode = os.system('copy /Y "%s" "%s"' % (file_item, target_file))
             else:
                 if rmt_mode == RmtMode.LINK:
-                    retcode = call(['ln', file_item, target_file])
+                    if platform.release().find("-z4-") and os.path.splitext(target_file)[-1].lower() in RMT_MEDIAEXT:
+                        tmp = "%s/%s" % (os.path.dirname(os.path.dirname(target_file)), os.path.basename(target_file))
+                        # log.info('ln %s %s ; mv %s %s' % (file_item, tmp, tmp, target_file))
+                        retcode = os.system('ln "%s" "%s" ; mv "%s" "%s"' % (file_item, tmp, tmp, target_file))
+                    else:
+                        retcode = call(['ln', file_item, target_file])
                 elif rmt_mode == RmtMode.SOFTLINK:
                     retcode = call(['ln', '-s', file_item, target_file])
                 else:
