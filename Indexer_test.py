ea63b0e46e623231147e4f4d016eb8ab418b0ed2
BeiMou <821204616@qq.com>
2022-08-18 09:22:32 +0800

    [update] 添加多站点容错

--- Indexer_test.py
+++ Indexer_test.py
@@ -6,35 +6,35 @@ Created on 2022-08-15 11:43:06
 ---------
 @author: YerWer
 """
+import cssselect
+import copy
+import dateparser
+import re
 import yaml
 import cssutils
 import feapder
+import querystring
 manual_cookies={}
 from pyquery import PyQuery as pq
-
 cookies_txt="c_secure_ssl=eWVhaA%3D%3D; c_secure_uid=Mzk5MTk%3D; c_secure_pass=948fc5b348732320a396c4ef7d322fb9; c_secure_tracker_ssl=eWVhaA%3D%3D; c_secure_login=bm9wZQ%3D%3D"
-cookiesdic=cookies_txt.split(';')
+
+mteamcookies="cf_clearance=frC9Wqm27XoWeTyh8Va3QitBqonOGgs0wpHNdzX3lhQ-1659945429-0-150; tp=MDRlNDQ3MTAwYWQ0MGIyZDg0N2M5YTI2Y2E2ZTZlZGU2ZTliM2M5ZQ%3D%3D"
+
+
+keepfrdscookies="c_secure_uid=MzU4NzA%3D; c_secure_pass=336bf06d014ef8bec9b72aa901c0d736; c_secure_ssl=eWVhaA%3D%3D; c_secure_tracker_ssl=eWVhaA%3D%3D; c_secure_login=bm9wZQ%3D%3D; _ga=GA1.2.1159861350.1651643224;"
+
+audiencescookies="c_secure_uid=MTYxNjY%3D; c_secure_pass=32644443b8d10d9f8904c5010d9fc3ae; c_secure_ssl=eWVhaA%3D%3D; c_secure_tracker_ssl=eWVhaA%3D%3D; c_secure_login=bm9wZQ%3D%3D;"
+
+cookiesdic=audiencescookies.split(';')
 import requests
 session = requests.session()
 
 #设置请求头信息
 for item in cookiesdic:
-    if item !='' :
+    if item !='':
         name,value=item.strip().split('=',1)  #用=号分割，分割1次
         manual_cookies[name]=value  #为字典cookies添加内容
 
-#将字典转为CookieJar：
-cookiesJar = requests.utils.cookiejar_from_dict(manual_cookies, cookiejar=None,overwrite=True)
-
-#将cookiesJar赋值给会话
-session.cookies=cookiesJar
-
-#向目标网站发起请求
-
-#将CookieJar转为字典：
-# res_cookies_dic = requests.utils.dict_from_cookiejar(res.cookies)
-'//*[@id="outer"]/table/tbody/tr/td/table[2]/tbody/tr[2]/td[3]/div[1]/a'
-xpath='//*[@id="outer"]/table/tbody/tr/td/table[2]/tbody/tr[2]/td[3]/div[2]'
 serchurl="https://lemonhd.org/torrents_movie.php?search=钢铁侠&search_area=name&column=g_last_upload_date&sort=asc&suggest=6"
 searchurl="https://lemonhd.org/torrents_movie.php?stype=s&search=%E9%92%A2%E9%93%81%E4%BE%A0&search_area=name&seed_count=&column=added&sort=asc&suggest=4"
 
@@ -76,13 +76,15 @@ searchurl="https://lemonhd.org/torrents_movie.php?stype=s&search=%E9%92%A2%E9%93
 class SpiderTest(feapder.AirSpider):
 
     def start_requests(self):
-        self.indexer=indexers('./nm.yml')
+        self.indexer=indexers('sites/audiences.yml')
         domain=self.indexer.domain
         torrentspath=  self.indexer.search['paths'][0]['path']
         searchurl=domain+torrentspath+'?stypes=s&search='+'钢铁侠'+'&search_area=name&seed_count=&column=added&sort=asc&suggest=4'
 
         testurl="https://lemonhd.org/torrents_new.php"
-        yield feapder.Request(searchurl,cookies=manual_cookies)
+        testurl3="https://kp.m-team.cc/movie.php"
+        testurl2=domain+"torrents.php"
+        yield feapder.Request(testurl2,cookies=manual_cookies)
 
     def detail_requests(self,download_url):
         searchurl=self.indexer.domain+download_url
@@ -103,17 +105,27 @@ class SpiderTest(feapder.AirSpider):
              if len(downloadvolumefactor)>0:
                 self.tor.downloadvolumefactor.append(self.fields['downloadvolumefactor']['case'][downloadvolumefactorselector])
                 break
-
+    def Getuploadvolumefactorselector(self,torrent):
+        self.tor.uploadvolumefactor=[]
+        for uploadvolumefactorselector in list(self.fields['uploadvolumefactor']['case'].keys()):
+             uploadvolumefactor=pq(torrent)(uploadvolumefactorselector)
+             if len(uploadvolumefactor)>0:
+                self.tor.uploadvolumefactor.append(self.fields['uploadvolumefactor']['case'][uploadvolumefactorselector])
+                break
     def Gettitle_default(self,torrent):
         # title_default
-        title_default = torrent(self.fields['title_default']['selector'])
+        if "title_default" in self.fields:
+            title_default = torrent(self.fields['title_default']['selector'])
+        else:
+            title_default = torrent(self.fields['title']['selector'])
         items = [item.text() for item in title_default.items()]
-        self.tor.title_default=items
+        self.tor.title=items
 
     def Getdetails(self,torrent):
         # details
         details=torrent(self.fields['details']['selector'])
         items = [item.attr(self.fields['details']['attribute']) for item in details.items()]
+        self.tor.page_url=self.indexer.domain+items[0]
         self.tor.details=items
 
     def Getdownload(self,torrent):
@@ -124,9 +136,11 @@ class SpiderTest(feapder.AirSpider):
 
     def Getimdbid(self,torrent):
         # imdb
-        imdbid = torrent(self.fields['imdbid']['selector'])
-        items = [item.attr(self.fields['imdbid']['attribute']) for item in imdbid.items()]
-        self.tor.imdbid=items
+        if "imdbid" in self.fields:
+            imdbid = torrent(self.fields['imdbid']['selector'])
+            items = [item.attr(self.fields['imdbid']['attribute']) for item in imdbid.items()]
+            self.tor.imdbid=items
+
 
     def Getsize(self,torrent):
         # torrent size
@@ -139,7 +153,13 @@ class SpiderTest(feapder.AirSpider):
         leechers=torrent(self.fields['leechers']['selector'])
         items = [item.text() for item in leechers.items()]
         self.tor.leechers=items
+        self.tor.peers=items
 
+    def Getseeders(self,torrent):
+        # torrent leechers
+        seeders=torrent(self.fields['seeders']['selector'])
+        items = [item.text() for item in seeders.items()]
+        self.tor.seeders=items
     def Getgrabs(self,torrent):
         # torrent grabs
         grabs=torrent(self.fields['grabs']['selector'])
@@ -147,11 +167,18 @@ class SpiderTest(feapder.AirSpider):
         self.tor.grabs=items
     def Gettitle_optional(self,torrent):
         # title_optional
-        t_o=torrent(self.fields['description']['selectors'])
-        items = [item.text() for item in t_o.items()]
-        self.tor.title_optional=[]
-        for i in range(int(len(items)/3)):
-            self.tor.title_optional.append(items[i*3+1])
+        selector=self.fields['description']
+        t_o=torrent(selector['selector'])
+        if "contents" in selector:
+            t_o=t_o("span")[selector["contents"]]
+            items=t_o.text
+
+
+        # items = [item.text() for item in t_o.items()]
+
+        # self.tor.description=[]
+        # for i in range(int(len(items)/3)):
+        self.tor.description=items
 
     def Getdate_added(self,torrent):
         # date_added
@@ -165,6 +192,7 @@ class SpiderTest(feapder.AirSpider):
         items = [item.text() for item in selector.items()]
         self.tor.date_elapsed=items
 
+
     def Getcategory(self,torrent):
         # date_added
         selector=torrent(self.fields['category']['selector'])
@@ -176,12 +204,38 @@ class SpiderTest(feapder.AirSpider):
                     arg2=filter['args'][1]
                     for i in range(len(items)):
                         items[i]=items[i].replace(arg1,arg2)
+                if filter['name']=="querystring":
+                    arg1=filter['args']
+                    str=items[0]
+                    str="car=daasd$dada=dasda"
+
+
+                    items=querystring.parse_qs(str)
+
         self.tor.category=items
+
 
     def Getfree_deadline(self,torrent):
         # date_added
         selector=torrent(self.fields['free_deadline']['selector'])
-        items = [item.text() for item in selector.items()]
+        # items = [item.text() for item in selector.items()]
+        items = [item.attr(self.fields['free_deadline']['attribute']) for item in selector.items()]
+        # print(items)
+        # print(len(items))
+        if len(items)>0 and items != None:
+            if items[0]!=None:
+                # print(len(items))
+                if "filters" in self.fields['free_deadline']:
+                     itemdata=items[0]
+                     for filter in self.fields['free_deadline']['filters']:
+                        if filter['name']=="re_search":
+                            arg1=filter['args'][0]
+                            arg2=filter['args'][1]
+                            items=re.search(arg1,itemdata,arg2)[0]
+                        if filter['name']=="dateparse":
+                            arg1=filter['args']
+                            items = dateparser.parse(items, date_formats=[arg1])
+
         self.tor.free_deadline=items
 
 
@@ -190,11 +244,14 @@ class SpiderTest(feapder.AirSpider):
         self.Gettitle_optional(torrent)
         self.Getgrabs(torrent)
         self.Getleechers(torrent)
+        self.Getseeders(torrent)
         self.Getsize(torrent)
         self.Getimdbid(torrent)
         self.Getdownload(torrent)
         self.Getdetails(torrent)
         self.Getdownloadvolumefactorselector(torrent)
+        self.Getuploadvolumefactorselector(torrent)
+
         self.Getdate_added(torrent)
         self.Getdate_elapsed(torrent)
         self.Getcategory(torrent)
@@ -207,90 +264,40 @@ class SpiderTest(feapder.AirSpider):
 
         print(response.xpath("//title/text()").extract_first())
         article_list = response.extract()
-
         # torrentlist=article_list.xpath('//table[@class="torrents"]/tr[acontains(@href,"download.php?")]')
         # detials=response.xpath('string(//a[@href="details_movie.php?id=276488"])')
         self.fields=self.indexer.torrents['fields']
-
         doc = pq(article_list)
+        # torrents=doc.css("table.torrents > tr:has(table.torrentname)")
+
+        # html_obj = etree.HTML(article_list)
+        # span = doc.cssselect(
+        # '.list > .four')[
+        # 0]
+
+
+
         # aa=doc('a[href^="download.php?"]')
+
 
 
         torrents=doc(self.indexer.torrents['list']['selector'])
         self.videos=[]
-
         self.tor=torrentclass()
         # title_default
 
-#outer > table > tbody > tr > td > table.torrents > tbody > tr:nth-child(2) > td:nth-child(3) > div:nth-child(2)
-#form_torrent > table > tbody > tr:nth-child(2) > td.torrenttr > table > tbody > tr > td:nth-child(2) > a > b
-#form_torrent > table > tbody > tr:nth-child(2) > td.torrenttr > table > tbody > tr > td:nth-child(2) > br
-
-
-
-
-        # downloadvolumefactorselector
-
-
-        # torrentslist=torrents("td")
         self.torrents_info=[]
         self.tor.downloadvolumefactor=[]
         for torn in torrents:
             torn=pq(torn)
-            self.torrents_info.append(self.Getinfo(torn))
+            self.tor.indexer=self.indexer.id
+            self.torrents_info.append(copy.deepcopy(self.Getinfo(torn)))
 
-
-
-        # items = [item.attr(self.fields['title_optional']['attribute']) for item in t_o.items()]
-
-
-        # self.title_optional=[]
-        # searchurl=self.indexer.domain+self.tor.details[0]
-
-        # yield feapder.Request(searchurl,callback=self.get_title_optional,cookies=manual_cookies) # 不指定callback，任务会调度默认的parser上
-
-
-        # title_optional
-        # self.detail_requests(self.tor.details[0])
-        # title_optional =self.title_optional
-        # items = [item.text() for item in title_optional.items()]
-        # tor.title_optional=items
-
-        # self.videos.append(tor)
-
-
-
-
-
-#        # detials = response.xpath('//a[starts-with(@href,"details_")]')
-#         download =response.xpath('//a[starts-with(@href,"download.php?")]')
-
-        # a[href^="download.php?"]
-        # titles=torrent.xpath('string(//a[@title][@href="details_movie.php?id=276488"])').extract_first()
-        # torrent.
-        # article_list = response.xpath('a[@herf="details_"]')
         html_data=response.xpath('//a[@herf="download.php"]')
-        # for torrent in html_data:
-        #     title = torrent.xpath("./text()").extract_first()
-        #     url = torrent.xpath("./@href").extract_first()
 
-        # content = response.xpath(
-        #     'string(//[@class="mainouter"])'
-        # ).extract_first()  # string 表达式是取某个标签下的文本，包括子标签文本
-        # 提取网站描述
-        # print(response.xpath("//torrents"))
         print("网站地址: ", response.url)
 
-# class GetTitleOptional(feapder.AirSpider):
-#     def start_requests(self,download_url):
-#         searchurl=self.indexer.domain+download_url
-#         yield feapder.Request(searchurl, cookies=manual_cookies)
-#
-#     def parse(self, request, response):
-#         t_o=response(self.fields['title_optional']['selector'])
-#         # item.attr(self.fields['title_optional']['attribute'])
-#         items = [item.attr(self.fields['title_optional']['attribute']) for item in t_o.items()]
-#         self.title_optional.append(items)
+
 
 
 
