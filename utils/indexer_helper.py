c0e4c9454c5e1b2a8a78b8238cc8f6fde37d47ff
jxxghp <jxxghp@163.com>
2022-08-21 16:21:21 +0800

    add sites

--- utils/indexer_helper.py
+++ utils/indexer_helper.py
@@ -1,9 +1,7 @@
 import os.path
+import pickle
 
-import yaml
-
-from config import Config
-from utils.functions import get_dir_files, singleton
+from utils.functions import singleton
 from urllib import parse
 
 from utils.http_utils import RequestUtils
@@ -13,18 +11,13 @@ from utils.indexer_conf import IndexerConf
 @singleton
 class IndexerHelper:
     _indexers = []
-    _site_path = None
 
     def __init__(self):
         self.init_config()
 
     def init_config(self):
-        # 站点配置文件在config文件夹sites目录下
-        self._site_path = os.path.join(os.path.dirname(Config().get_config_path()), "sites")
-        cfg_files = get_dir_files(in_path=self._site_path, exts=[".yml"])
-        for cfg_file in cfg_files:
-            with open(cfg_file, mode='r', encoding='utf-8') as f:
-                self._indexers.append(yaml.safe_load(f))
+        with open(os.path.join(os.getcwd(), "config", "sites.dat"), "rb") as f:
+            self._indexers = pickle.load(f)
 
     def get_all_indexers(self):
         return self._indexers
