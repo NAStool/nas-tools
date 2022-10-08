47a1e0882c6965852dd012cc9c6520d11bed5800
jxxghp <jxxghp@163.com>
2022-10-08 10:58:30 +0800

    bug fix

--- app/helper/chrome_helper.py
+++ app/helper/chrome_helper.py
@@ -24,6 +24,7 @@ class ChromeHelper(object):
         if self._chrome:
             self._chrome.quit()
         if not Config().get_config('laboratory').get('chrome_browser'):
+            self._chrome = None
             return
         if SystemUtils.get_system() == OsType.LINUX \
                 and self._executable_path \
