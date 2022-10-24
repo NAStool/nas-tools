66594aa5626db6ee4098ec9e97ca06696114dd14
jxxghp <jxxghp@163.com>
2022-10-24 08:29:56 +0800

    mp4ba render

--- app/sites/siteconf.py
+++ app/sites/siteconf.py
@@ -407,7 +407,8 @@ class SiteConf:
             "proxy": False
         },
         'www.mp4ba.vip': {
-            "proxy": False
+            "proxy": False,
+            "render": True
         },
         'www.miobt.com': {
             "proxy": True
