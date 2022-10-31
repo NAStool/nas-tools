aeaa5a20ec5e9c36e6fb0a802a8ff2cbb434803d
Azure99 <961523404@qq.com>
2022-11-01 02:11:49 +0800

    修复传入的地址重定向到magnet链接时, 获取种子内容失败的问题

--- app/utils/torrent.py
+++ app/utils/torrent.py
@@ -71,7 +71,12 @@ class Torrent:
         if url.startswith("magnet:"):
             return url, "磁力链接"
         try:
-            req = RequestUtils(headers=ua, cookies=cookie, referer=referer).get_res(url=url)
+            req = RequestUtils(headers=ua, cookies=cookie, referer=referer).get_res(url=url, allow_redirects=False)
+            while req and req.status_code in [301, 302]:
+                url = req.headers['Location']
+                if url.startswith("magnet:"):
+                    return url, "磁力链接"
+                req = RequestUtils(headers=ua, cookies=cookie, referer=referer).get_res(url=url, allow_redirects=False)
             if req and req.status_code == 200:
                 if not req.content:
                     return None, "未下载到种子数据"
