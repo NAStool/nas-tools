20b14deee645587fe302cf873214bf156e3239fb
jxxghp <jxxghp@163.com>
2022-11-15 16:25:01 +0800

    兼容IPV6旧监听配置

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,9 +7,11 @@ from config import Config
 # 这里读取配置文件进行启动项参数的入参
 app_conf = Config().get_config('app')
 if app_conf:
-    _web_host = app_conf.get("web_host") if app_conf.get("web_host") else '[::]'
+    _web_host = app_conf.get("web_host")
+    if not _web_host or _web_host == '::':
+        _web_host = '[::]'
     _web_port = int(app_conf.get('web_port')) if str(app_conf.get('web_port')).isdigit() else 3000
-    bind = f"{_web_host}:{_web_port}"
+    bind = f"{_web_host}:{int(_web_port)}"
     keyfile = app_conf.get('ssl_cert')
     certfile = app_conf.get('ssl_key')
 
