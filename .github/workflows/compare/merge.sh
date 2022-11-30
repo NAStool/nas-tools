a39d4e5b7206964cb56c3bb08d9d2663cffa6933
sungamma <sungamma@gmail.com>
2022-11-30 21:20:36 +0800

    调整文件名称，workflow名称

--- .github/workflows/compare/merge.sh
+++ .github/workflows/compare/merge.sh
@@ -1,13 +1,13 @@
-diff build.yml compare/build.yml.bak > /dev/null
+diff build-windows.yml compare/build-windows.yml.bak > /dev/null
 if [  "$?" == "0" ]; then #"$?"是上一执行命令的返回值。
     echo "nothing to change"
     else
     sudo snap install yq
-    cp build.yml build-2.yml #制作实时更新的带有修改记录的action文件
-    cp build.yml compare/build.yml.bak #为下次比较否有变化做准备
-    myenv=$(cat compare/tags-build) yq -i '.jobs.Create-release_Send-message=env(myenv)' build-2.yml  # 把生成修改记录的action放入build-2.yml
-    yq -i '.name="Nas-tools Build-2"' build-2.yml
-    cat build-2.yml
+    cp build-windows.yml build-windows-2.yml #制作实时更新的带有修改记录的action文件
+    cp build-windows.yml compare/build-windows.yml.bak #为下次比较否有变化做准备
+    myenv=$(cat compare/tags-build) yq -i '.jobs.Create-release_Send-message=env(myenv)' build-windows-2.yml  # 把生成修改记录的action放入build-2.yml
+    yq -i '.name="Build NAStool Windows-2"' build-windows-2.yml
+    cat build-windows-2.yml
     git add .
     git commit -m "update build-2.yml"
     fi
