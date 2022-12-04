7c8395ddd3d424ec549274441dc855635ec317f1
percent73 <6688327+geziang@users.noreply.github.com>
2022-12-04 13:47:31 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -75,4 +75,10 @@ mkdir -p /.pm2
 chown -R ${PUID}:${PGID} ${WORKDIR} /config /usr/lib/chromium /.local /.pm2
 export PATH=$PATH:/usr/lib/chromium
 umask ${UMASK}
-exec su-exec ${PUID}:${PGID} pm2-runtime start run.py -n NAStool --interpreter python3
+
+PUID=${PUID:-911}
+PGID=${PGID:-911}
+
+groupmod -o -g "$PGID" abc
+usermod -o -u "$PUID" abc
+exec su abc pm2-runtime start run.py -n NAStool --interpreter python3
