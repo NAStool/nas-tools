520e5b3c2ef4134702551290a149f9147fd91c56
jxxghp <jxxghp@163.com>
2022-12-09 19:48:00 +0800

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -75,4 +75,4 @@ mkdir -p /.pm2
 chown -R ${PUID}:${PGID} ${WORKDIR} /config /usr/lib/chromium /.local /.pm2
 export PATH=$PATH:/usr/lib/chromium
 umask ${UMASK}
-exec su-exec ${PUID}:${PGID} pm2-runtime start run.py -n NAStool --interpreter python3
+su-exec ${PUID}:${PGID} pm2-runtime start run.py -n NAStool --interpreter python3
