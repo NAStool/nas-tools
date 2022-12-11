b18212a5babeb457d94dbec8e1cab872f7fea341
jxxghp <jxxghp@163.com>
2022-12-11 11:55:20 +0800

    搜索结果过滤

--- config.py
+++ config.py
@@ -63,11 +63,11 @@ TORRENT_SEARCH_PARAMS = {
     "restype": {
         "BLURAY": r"Blu-?Ray|BD|BDRIP",
         "REMUX": r"REMUX",
-        "DOLBY": r"DOLBY",
+        "DOLBY": r"DOLBY|DOVI",
         "WEB": r"WEB-?DL|WEBRIP",
         "HDTV": r"U?HDTV",
         "UHD": r"UHD",
-        "HDR": r"HDR",
+        "HDR": r"HDR|HDR10",
         "3D": r"3D"
     },
     "pix": {
