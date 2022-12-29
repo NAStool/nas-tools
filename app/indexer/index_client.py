5ddcb3b4ce6a1b2181fc29e94b44076815ae6593
jxxghp <jxxghp@163.com>
2022-12-29 16:40:14 +0800

    fix tjupt

--- app/indexer/index_client.py
+++ app/indexer/index_client.py
@@ -19,7 +19,6 @@ class IIndexClient(metaclass=ABCMeta):
     filter = None
     progress = None
     _reverse_title_sites = ['keepfriends']
-    _invalid_description_sites = ['tjupt']
 
     def __init__(self):
         self.media = Media()
@@ -210,9 +209,6 @@ class IIndexClient(metaclass=ABCMeta):
             else:
                 torrent_name = item.get('title')
                 description = item.get('description')
-            # 这些站副标题无意义，需要去除
-            if indexer.id in self._invalid_description_sites:
-                description = ""
             if not torrent_name:
                 index_error += 1
                 continue
