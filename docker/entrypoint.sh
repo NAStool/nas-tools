14b9b450b9ed7a3c51eb976df917a22758e6d002
accors <qsxdfc@126.com>
2023-01-04 10:22:05 +0000

    删除package_list_lite，修改entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -18,7 +18,7 @@ if [ "$NASTOOL_AUTO_UPDATE" = "true" ]; then
     git clean -dffx
     git reset --hard HEAD
     git pull
-    if [ $? -eq 0 ]; then
+        if [ $? -eq 0 ]; then
         echo "更新成功..."
         # Python依赖包更新
         hash_old=$(cat /tmp/requirements.txt.sha256sum)
@@ -47,24 +47,6 @@ if [ "$NASTOOL_AUTO_UPDATE" = "true" ]; then
             fi
         fi
         # 系统软件包更新
-    if [ "$NASTOOL_VERSION" = "lite" ]; then
-        hash_old=$(cat /tmp/package_list_lite.txt.sha256sum)
-        hash_new=$(sha256sum package_list_lite.txt)
-        if [ "$hash_old" != "$hash_new" ]; then
-            echo "检测到package_list_lite.txt有变化，更新软件包..."
-            apk add --no-cache libffi-dev
-            apk add --no-cache $(echo $(cat package_list_lite.txt))
-            if [ $? -ne 0 ]; then
-                echo "无法更新软件包，请更新镜像..."
-            else
-                apk del libffi-dev
-                echo "软件包安装成功..."
-                sha256sum package_list_lite.txt > /tmp/package_list_lite.txt.sha256sum
-            fi
-        fi
-    fi
-    else
-    if [ ! -n "$NASTOOL_VERSION" ]; then
         hash_old=$(cat /tmp/package_list.txt.sha256sum)
         hash_new=$(sha256sum package_list.txt)
         if [ "$hash_old" != "$hash_new" ]; then
@@ -82,11 +64,10 @@ if [ "$NASTOOL_AUTO_UPDATE" = "true" ]; then
     else
         echo "更新失败，继续使用旧的程序来启动..."
     fi
-fi
 else
     echo "程序自动升级已关闭，如需自动升级请在创建容器时设置环境变量：NASTOOL_AUTO_UPDATE=true"
-    fi
- echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序..."
+fi
+echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序..."
 mkdir -p /.local
 mkdir -p /.pm2
 if [ "$NASTOOL_VERSION" = "lite" ]; then
