8932cc80da0e895e8d5887e1e6ac00238ef3e863
jxxghp <jxxghp@163.com>
2023-01-05 14:57:34 +0800

    fix update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 cd ${WORKDIR}
-if [ "$NASTOOL_AUTO_UPDATE" = "true" ]; then
+if [ "${NASTOOL_AUTO_UPDATE}" = "true" ]; then
     if [ ! -s /tmp/requirements.txt.sha256sum ]; then
         sha256sum requirements.txt > /tmp/requirements.txt.sha256sum
     fi
     if [ ! -s /tmp/third_party.txt.sha256sum ]; then
         sha256sum third_party.txt > /tmp/third_party.txt.sha256sum
     fi
-    if [ "$NASTOOL_VERSION" != "lite" ]; then
+    if [ "${NASTOOL_VERSION}" != "lite" ]; then
         if [ ! -s /tmp/package_list.txt.sha256sum ]; then
             sha256sum package_list.txt > /tmp/package_list.txt.sha256sum
         fi
@@ -17,17 +17,22 @@ if [ "$NASTOOL_AUTO_UPDATE" = "true" ]; then
     git remote set-url origin "${REPO_URL}" &> /dev/null
     echo "windows/" > .gitignore
     echo "third_party/feapder/feapder/network/proxy_file/" >> .gitignore
+    if [ "${NASTOOL_VERSION}" == "dev" ]; then
+      branch="dev"
+    else
+      branch="master"
+    fi
     git clean -dffx
-    git reset --hard HEAD
-    git pull --depth=1
+    git fetch --depth 1 origin ${branch}
+    git reset --hard origin/${branch}
     if [ $? -eq 0 ]; then
         echo "更新成功..."
         # Python依赖包更新
         hash_old=$(cat /tmp/requirements.txt.sha256sum)
         hash_new=$(sha256sum requirements.txt)
-        if [ "$hash_old" != "$hash_new" ]; then
+        if [ "${hash_old}" != "${hash_new}" ]; then
             echo "检测到requirements.txt有变化，重新安装依赖..."
-            if [ "$NASTOOL_CN_UPDATE" = "true" ]; then
+            if [ "${NASTOOL_CN_UPDATE}" = "true" ]; then
                 pip install --upgrade pip setuptools wheel -i https://pypi.tuna.tsinghua.edu.cn/simple
                 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
             else
@@ -41,7 +46,7 @@ if [ "$NASTOOL_AUTO_UPDATE" = "true" ]; then
                 sha256sum requirements.txt > /tmp/requirements.txt.sha256sum
                 hash_old=$(cat /tmp/third_party.txt.sha256sum)
                 hash_new=$(sha256sum third_party.txt)
-                if [ "$hash_old" != "$hash_new" ]; then
+                if [ "${hash_old}" != "${hash_new}" ]; then
                     echo "检测到third_party.txt有变化，更新第三方组件..."
                     git submodule update --init --recursive
                     if [ $? -ne 0 ]; then
@@ -54,12 +59,12 @@ if [ "$NASTOOL_AUTO_UPDATE" = "true" ]; then
             fi
         fi
         # 系统软件包更新
-        if [ "$NASTOOL_VERSION" != "lite" ]; then
+        if [ "${NASTOOL_VERSION}" != "lite" ]; then
             hash_old=$(cat /tmp/package_list.txt.sha256sum)
             hash_new=$(sha256sum package_list.txt)
-            if [ "$hash_old" != "$hash_new" ]; then
+            if [ "${hash_old}" != "${hash_new}" ]; then
                 echo "检测到package_list.txt有变化，更新软件包..."
-                if [ "$NASTOOL_CN_UPDATE" = "true" ]; then
+                if [ "${NASTOOL_CN_UPDATE}" = "true" ]; then
                     sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
                     apk update -f
                 fi
@@ -83,14 +88,14 @@ fi
 
 echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序..."
 
-if [ "$NASTOOL_VERSION" = "lite" ]; then
+if [ "${NASTOOL_VERSION}" = "lite" ]; then
     mkdir -p /.pm2
     chown -R "${PUID}":"${PGID}" "${WORKDIR}" /config /.pm2
 else
     mkdir -p /.local
     mkdir -p /.pm2
     chown -R "${PUID}":"${PGID}" "${WORKDIR}" /config /usr/lib/chromium /.local /.pm2
-    export PATH=$PATH:/usr/lib/chromium
+    export PATH=${PATH}:/usr/lib/chromium
 fi
 umask "${UMASK}"
 exec su-exec "${PUID}":"${PGID}" "$(which dumb-init)" "$(which pm2-runtime)" start run.py -n NAStool --interpreter python3
