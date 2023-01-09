1b8ef7cd897d2f22e417b438cdd6866043a12fcc
Shurelol <sjtumaxuewen@gmail.com>
2023-01-09 12:23:35 +0800

    fix 修复蜜柑rss文件大小解析，增加nyaa解析

--- app/utils/string_utils.py
+++ app/utils/string_utils.py
@@ -164,22 +164,26 @@ class StringUtils:
     @staticmethod
     def str_filesize(size, pre=2):
         """
-        将字节计算为文件大小描述
+        将字节计算为文件大小描述（带单位的格式化后返回）
         """
-        if not isinstance(size, int) or not isinstance(size, float):
+        size = re.sub(r"\s|B|iB", "", size, re.I)
+        if size.replace(".", "").isdigit():
             try:
                 size = float(size)
+                d = [(1024 - 1, 'K'), (1024 ** 2 - 1, 'M'), (1024 ** 3 - 1, 'G'), (1024 ** 4 - 1, 'T')]
+                s = [x[0] for x in d]
+                index = bisect.bisect_left(s, size) - 1
+                if index == -1:
+                    return str(size)
+                else:
+                    b, u = d[index]
+                return str(round(size / (b + 1), pre)) + u
             except Exception as e:
                 ExceptionUtils.exception_traceback(e)
                 return ""
-        d = [(1024 - 1, 'K'), (1024 ** 2 - 1, 'M'), (1024 ** 3 - 1, 'G'), (1024 ** 4 - 1, 'T')]
-        s = [x[0] for x in d]
-        index = bisect.bisect_left(s, size) - 1
-        if index == -1:
-            return str(size)
-        else:
-            b, u = d[index]
-        return str(round(size / (b + 1), pre)) + u
+        if re.findall(r"[KMGTP]", size, re.I):
+            return size
+        return ""
 
     @staticmethod
     def url_equal(url1, url2):
