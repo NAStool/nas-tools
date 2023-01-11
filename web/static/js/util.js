ee607748f87df4113f07454f9a7c2df39196fa96
jxxghp <jxxghp@163.com>
2023-01-11 12:15:03 +0800

    优化 搜索特殊字符处理/版本号比较

--- web/static/js/util.js
+++ web/static/js/util.js
@@ -273,3 +273,32 @@ function bytesToSize(bytes) {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+/**
+ * 比较版本号大小，v1大于v2时返回1，相等返回0，否则返回-1
+ */
+function compareVersion(v1, v2) {
+    v1 = v1.split('-')[0].split('.');
+    v2 = v2.split('-')[0].split('.');
+    const len = Math.max(v1.length, v2.length);
+
+    while (v1.length < len) {
+        v1.push('0');
+    }
+    while (v2.length < len) {
+        v2.push('0');
+    }
+
+    for (let i = 0; i < len; i++) {
+        const num1 = parseInt(v1[i]);
+        const num2 = parseInt(v2[i]);
+
+        if (num1 > num2) {
+            return 1;
+        } else if (num1 < num2) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
