1a67d4632fe019491fa9b84673360654d14ccb4e
叮叮当 <604054726@qq.com>
2023-01-16 14:13:02 +0800

    Lit重写card/placeholder/img  引入lit-core/lit-state包

---
+++ web/static/components/normal-card/state.js
@@ -0,0 +1,11 @@
+import { LitState } from "../utility/lit-state.js"
+
+class CardState extends LitState {
+    static get stateVars() {
+        return {
+            more_id: undefined
+        };
+    }
+}
+
+export const cardState = new CardState();
