374ee820e971a6fe00866504798c91498c1a9415
jxxghp <51039935+jxxghp@users.noreply.github.com>
2023-01-27 11:48:12 +0800

    更新 mediainfo.js

--- web/static/components/page/mediainfo.js
+++ web/static/components/page/mediainfo.js
@@ -123,20 +123,20 @@ class PageMediainfo extends CustomElement {
                       ${this.fav == "2"
                       ? html`<strong class="badge badge-pill bg-green text-white">已下载</strong>`
                       : html`
-                        <a class="btn btn-outline-indigo btn-pill me-1"
+                        <a class="btn btn-outline-indigo me-1"
                           href='javascript:media_search("${this.tmdbid}", "${this.media_info.title}", "${this.media_type}")'>
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="10" cy="10" r="7"></circle><line x1="21" y1="21" x2="15" y2="15"></line></svg>
                           搜索资源
                         </a>
                         ${this.fav == "0"
                         ? html`
-                          <span class="btn btn-outline-pink btn-pill"
+                          <span class="btn btn-outline-pink"
                             @click=${this._loveClick}>
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
                             添加订阅
                           </span>`
                         : html`
-                          <span class="btn btn-outline-pinterest btn-pill"
+                          <span class="btn btn-outline-pinterest"
                             @click=${this._loveClick}>
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
                             删除订阅
