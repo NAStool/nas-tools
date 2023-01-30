937476d7d5e3684f9bb97404372c39c44b4b3881
叮叮当 <604054726@qq.com>
2023-01-30 14:07:12 +0800

    fix 推荐页面也显示媒体类型 效果是否会比较好?

--- web/static/components/page/discovery.js
+++ web/static/components/page/discovery.js
@@ -1,4 +1,4 @@
-import { html } from "../utility/lit-core.min.js";
+import { html, nothing } from "../utility/lit-core.min.js";
 import { CustomElement, Golbal } from "../utility/utility.js";
 
 class PageDiscovery extends CustomElement {
@@ -16,48 +16,59 @@ class PageDiscovery extends CustomElement {
         {
           title:"TMDB最新电视剧",
           subtype :"nt",
+          mediatype: "电视剧",
         },
         {
           title:"TMDB热门电视剧",
           subtype :"ht",
+          mediatype: "电视剧",
         },
         {
           title:"豆瓣热门电视剧",
           subtype :"dbht",
+          mediatype: "电视剧",
         },
         {
           title:"豆瓣热门动漫",
           subtype :"dbdh",
+          mediatype: "动漫",
         },
         {
           title:"豆瓣热门综艺",
           subtype :"dbzy",
+          mediatype: "综艺",
         }
       ],
       "MOV":[
         {
           title:"正在热映",
           subtype :"dbom",
+          mediatype: "电影",
         },
         {
           title:"即将上映",
           subtype :"dbnm",
+          mediatype: "电影",
         },
         {
           title:"TMDB最新电影",
           subtype :"nm",
+          mediatype: "电影",
         },
         {
           title:"TMDB热门电影",
           subtype :"hm",
+          mediatype: "电影",
         },
         {
           title:"豆瓣热门电影",
           subtype :"dbhm",
+          mediatype: "电影",
         },
         {
           title:"豆瓣电影TOP250",
           subtype :"dbtop",
+          mediatype: "电影",
         }
       ],
       "BANGUMI": [
@@ -130,6 +141,8 @@ class PageDiscovery extends CustomElement {
                   card-year=${card.year}
                   card-title=${card.title}
                   card-overview=${card.overview}
+                  card-restype=${item.mediatype ?? nothing}
+                  card-weekday=${item.week ? item.title : nothing}
                   class="px-2"
                 ></normal-card>`))
               : Array(20).fill(html`<normal-card-placeholder></normal-card-placeholder>`)
