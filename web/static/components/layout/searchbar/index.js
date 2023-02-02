de7977693c2dc687695528363db53f659ac44f73
叮叮当 <604054726@qq.com>
2023-02-02 21:12:54 +0800

    fix 切换明亮模式的按钮

--- web/static/components/layout/searchbar/index.js
+++ web/static/components/layout/searchbar/index.js
@@ -95,7 +95,11 @@ export class LayoutSearchbar extends CustomElement {
         // 强制为dark
         dark = true;
         this.setAttribute("style",`background-color: rgba(${bg_black},${opacity})!important; ${blur_filter}`);
+        // 当前强制dark主题, so 必须强行显示切换明亮模式
+        this._chang_color = true;
       } else if (window_width >= 992 && dark) {
+        // 恢复默认设置
+        this._chang_color = false;
         // lg及以上
         screen_lg = true;
         this.classList.remove("theme-dark");
