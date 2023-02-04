0d5070129989eb5f7271e66343778280ff8902ca
jxxghp <51039935+jxxghp@users.noreply.github.com>
2023-02-04 18:35:20 +0800

    更新 index.js

--- web/static/components/layout/navbar/index.js
+++ web/static/components/layout/navbar/index.js
@@ -705,15 +705,6 @@ export class LayoutNavbar extends CustomElement {
       </style>
       <div class="container-fluid">
         <div class="offcanvas offcanvas-start d-flex lit-navbar-canvas shadow" tabindex="-1" id="litLayoutNavbar">
-          <div class="lit-navar-close d-lg-none">
-            <button type="button" class="btn btn-lg btn-ghost-light p-1" data-bs-dismiss="offcanvas" aria-label="Close">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x m-0" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M18 6l-12 12"></path>
-                <path d="M6 6l12 12"></path>
-              </svg>
-            </button>
-          </div>
           <div class="d-flex flex-row flex-grow-1 lit-navbar-hide-scrollbar">
             <div class="d-flex flex-column flex-grow-1">
               <h1 class="mt-3" style="text-align:center;">
