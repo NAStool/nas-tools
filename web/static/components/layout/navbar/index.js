047a388118ce24ac56d9dc8f3d98aa6102f2cd23
jxxghp <51039935+jxxghp@users.noreply.github.com>
2023-02-04 18:32:50 +0800

    更新 index.js

--- web/static/components/layout/navbar/index.js
+++ web/static/components/layout/navbar/index.js
@@ -1,3 +1,4 @@
+import { LayoutNavbarButton } from "./button.js"; export { LayoutNavbarButton };
 import { html, nothing } from "../../utility/lit-core.min.js";
 import { CustomElement } from "../../utility/utility.js";
 
