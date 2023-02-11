c2113ce0f85cc211e2fbd306c6cd30a732f955c0
jxxghp <jxxghp@163.com>
2023-02-11 17:57:34 +0800

    fix sort

--- web/action.py
+++ web/action.py
@@ -2420,7 +2420,7 @@ class WebAction:
             # 参数
             params = data.get("params") or {}
             # 排序
-            sort = params.get("sort") or "U"
+            sort = params.get("sort") or "T"
             # 选中的分类
             tags = params.get("tags") or ""
             # 过滤参数
