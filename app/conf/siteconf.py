82fe2f4306cf9dc89c1eb58caf30c6a4aa0eb5bd
jxxghp <jxxghp@163.com>
2023-02-11 08:47:01 +0800

    fix #2859

--- app/conf/siteconf.py
+++ app/conf/siteconf.py
@@ -537,7 +537,7 @@ class SiteConf:
         'www.hdpianyuan.com': {
             "proxy": False
         },
-        'skrbtfi.top': {
+        'skrbtla.top': {
             "proxy": False,
             "referer": True,
             "parser": "RenderSpider"
