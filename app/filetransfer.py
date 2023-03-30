e2e8e63d68304dc262e07a123194e8c2aa6c87de
usnail <usnail@yeah.net>
2023-03-30 23:55:34 +0800

    fix: 修复原盘电影削刮nfo无法识别的问题 #3917
    Signed-off-by: usnail <usnail@yeah.net>

--- app/filetransfer.py
+++ app/filetransfer.py
@@ -789,10 +789,17 @@ class FileTransfer:
                         message_medias[message_key].total_episodes += media.total_episodes
                         message_medias[message_key].size += media.size
                 # 生成nfo及poster
-                self.scraper.gen_scraper_files(media=media,
-                                               dir_path=ret_dir_path,
-                                               file_name=os.path.basename(ret_file_path),
-                                               file_ext=file_ext)
+                if bluray_disk_dir and media.type == MediaType.MOVIE:
+                    # 原盘文件的情况下 使用目录名称.nfo 生成
+                    self.scraper.gen_scraper_files(media=media,
+                                                   dir_path=ret_dir_path,
+                                                   file_name=os.path.basename(ret_dir_path),
+                                                   file_ext=file_ext)
+                else:
+                    self.scraper.gen_scraper_files(media=media,
+                                                   dir_path=ret_dir_path,
+                                                   file_name=os.path.basename(ret_file_path),
+                                                   file_ext=file_ext)
                 # 更新进度
                 self.progress.update(ptype=ProgressKey.FileTransfer,
                                      value=round(total_count / len(Medias) * 100),
