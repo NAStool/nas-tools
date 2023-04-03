34c38e8c62695ff2db8c29479e3252c2c3de79f5
usnail <usnail@yeah.net>
2023-04-03 23:42:37 +0800

    本地cmd-dialog
    Signed-off-by: usnail <usnail@yeah.net>

--- web/static/components/lit-index.js
+++ web/static/components/lit-index.js
@@ -4,3 +4,4 @@ export * from "./page/index.js";
 export * from "./layout/index.js";
 export * from "./plugin/index.js";
 export * from "./accordion/index.js";
+export * from "./cmd-dialog/index.js";
