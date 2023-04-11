7768d6ba5b182b33020d258ff650b12bbecb242a
Shurelol <sjtumaxuewen@gmail.com>
2023-04-11 19:35:37 +0800

    fix 优化cookiecloud同步日志

--- app/plugins/modules/cookiecloud.py
+++ app/plugins/modules/cookiecloud.py
@@ -243,9 +243,13 @@ class CookieCloud(_IPluginModule):
         """
         # 同步数据
         self.info(f"同步服务开始 ...")
-        contents, msg, _ = self.__download_data()
-        if not contents:
+        contents, msg, flag = self.__download_data()
+        if not flag:
             log.error(msg)
+            self.__send_message(msg)
+            return
+        if not contents:
+            self.info(f"未从CookieCloud获取到数据")
             self.__send_message(msg)
             return
         # 整理数据,使用domain域名的最后两级作为分组依据
