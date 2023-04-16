ac1968595bbeae307ba96271de06c7a8a1fc6e13
jxxghp <jxxghp@163.com>
2023-04-16 15:15:32 +0800

    os._exit增加注释

--- run.py
+++ run.py
@@ -47,6 +47,8 @@ def sigal_handler(num, stack):
     WebAction.stop_service()
     # 退出主进程
     log.info('退出主进程...')
+    # sys.exit(0) -> os._exit(0)
+    # fix s6下python进程无法退出的问题
     os._exit(0)
 
 
