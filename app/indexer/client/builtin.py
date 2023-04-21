91aa2c12a0d5b17a79b931eac1ca3c78d4adef61
jxxghp <jxxghp@163.com>
2023-04-21 13:20:32 +0800

    fix bug

--- app/indexer/client/builtin.py
+++ app/indexer/client/builtin.py
@@ -91,14 +91,14 @@ class BuiltinIndexer(_IIndexClient):
             for indexer in IndexerHelper().get_all_indexers():
                 if not indexer.get("public"):
                     continue
-                if check and indexer_sites and indexer.get("id") not in indexer_sites:
-                    continue
                 if indexer_id and indexer.get("id") == indexer_id:
                     return IndexerConf(datas=indexer)
+                if check and indexer_sites and indexer.get("id") not in indexer_sites:
+                    continue
                 if indexer.get("domain") not in _indexer_domains:
                     _indexer_domains.append(indexer.get("domain"))
                     ret_indexers.append(IndexerConf(datas=indexer))
-        return ret_indexers
+        return None if indexer_id else ret_indexers
 
     def search(self, order_seq,
                indexer,
