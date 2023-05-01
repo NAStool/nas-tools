893532fcdc325087dc2a621bc2799f9e5da8d513
jxxghp <jxxghp@163.com>
2023-05-01 18:23:06 +0800

    fix #4575

--- app/helper/site_helper.py
+++ app/helper/site_helper.py
@@ -25,13 +25,15 @@ class SiteHelper:
         if html.xpath("//input[@type='password']"):
             return False
         # 是否存在登出和用户面板等链接
-        logout_or_usercp = html.xpath('//a[contains(@href, "logout") or contains(@data-url, "logout")'
-                                      ' or contains(@href, "mybonus") '
-                                      ' or contains(@onclick, "logout") or contains(@href, "usercp")]')
-
-        if logout_or_usercp:
-            return True
-
+        xpaths = ['//a[contains(@href, "logout")'
+                  ' or contains(@data-url, "logout")'
+                  ' or contains(@href, "mybonus") '
+                  ' or contains(@onclick, "logout")'
+                  ' or contains(@href, "usercp")]',
+                  '//form[contains(@action, "logout")]']
+        for xpath in xpaths:
+            if html.xpath(xpath):
+                return True
         user_info_div = html.xpath('//div[@class="user-info-side"]')
         if user_info_div:
             return True
