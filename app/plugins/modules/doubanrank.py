5f652e7ca1de2570231ca18f447ef4bbf45fddfb
jxxghp <jxxghp@163.com>
2023-05-05 12:31:59 +0800

    豆瓣榜单订阅增加历史记录

--- app/plugins/modules/doubanrank.py
+++ app/plugins/modules/doubanrank.py
@@ -6,6 +6,7 @@ from threading import Event
 import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from jinja2 import Template
 
 from app.helper import RssHelper
 from app.mediaserver import MediaServer
@@ -227,6 +228,134 @@ class DoubanRank(_IPluginModule):
             }
         ]
 
+    def get_page(self):
+        """
+        插件的额外页面，返回页面标题和页面内容
+        :return: 标题，页面内容，确定按钮响应函数
+        """
+        results = self.get_history()
+        template = """
+             <div class="table-responsive table-modal-body">
+               <table class="table table-vcenter card-table table-hover table-striped">
+                 <thead>
+                 <tr>
+                   <th></th>
+                   <th>标题</th>
+                   <th>类型</th>
+                   <th>状态</th>
+                   <th>添加时间</th>
+                   <th></th>
+                 </tr>
+                 </thead>
+                 <tbody>
+                 {% if HistoryCount > 0 %}
+                   {% for Item in DoubanRankHistory %}
+                     <tr id="movie_rank_history_{{ Item.id }}">
+                       <td class="w-5">
+                         <img class="rounded w-5" src="{{ Item.image }}"
+                              onerror="this.src='../static/img/no-image.png'" alt=""
+                              style="min-width: 50px"/>
+                       </td>
+                       <td>
+                         <div>{{ Item.name }} ({{ Item.year }})</div>
+                         {% if Item.rating %}
+                           <div class="text-muted text-nowrap">
+                           评份：{{ Item.rating }}
+                           </div>
+                         {% endif %}
+                       </td>
+                       <td>
+                         {{ Item.type }}
+                       </td>
+                       <td>
+                         {% if Item.state == 'DOWNLOADED' %}
+                           <span class="badge bg-green">已下载</span>
+                         {% elif Item.state == 'RSS' %}
+                           <span class="badge bg-blue">已订阅</span>
+                         {% elif Item.state == 'NEW' %}
+                           <span class="badge bg-blue">新增</span>
+                         {% else %}
+                           <span class="badge bg-orange">处理中</span>
+                         {% endif %}
+                       </td>
+                       <td>
+                         <small>{{ Item.add_time or '' }}</small>
+                       </td>
+                       <td>
+                         <div class="dropdown">
+                           <a href="#" class="btn-action" data-bs-toggle="dropdown"
+                              aria-expanded="false">
+                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots-vertical {{ class }}"
+                                  width="24" height="24" viewBox="0 0 24 24"
+                                  stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                               <circle cx="12" cy="12" r="1"></circle>
+                               <circle cx="12" cy="19" r="1"></circle>
+                               <circle cx="12" cy="5" r="1"></circle>
+                             </svg>
+                           </a>
+                           <div class="dropdown-menu dropdown-menu-end">
+                             <a class="dropdown-item text-danger"
+                                href='javascript:DoubanRank_delete_history("{{ Item.id }}")'>
+                               删除
+                             </a>
+                           </div>
+                         </div>
+                       </td>
+                     </tr>
+                   {% endfor %}
+                 {% else %}
+                   <tr>
+                     <td colspan="6" align="center">没有数据</td>
+                   </tr>
+                 {% endif %}
+                 </tbody>
+               </table>
+             </div>
+           """
+        return "订阅历史", Template(template).render(HistoryCount=len(results),
+                                                     DoubanRankHistory=results), None
+
+    @staticmethod
+    def get_script():
+        """
+        删除随机电影历史记录的JS脚本
+        """
+        return """
+          // 删除随机电影历史记录
+          function DoubanRank_delete_history(id){
+            ajax_post("run_plugin_method", {"plugin_id": 'DoubanRank', 'method': 'delete_rank_history', 'tmdb_id': id}, function (ret) {
+              $("#movie_rank_history_" + id).remove();
+            });
+
+          }
+        """
+
+    def delete_rank_history(self, tmdb_id):
+        """
+        删除同步历史
+        """
+        return self.delete_history(key=tmdb_id)
+
+    def __update_history(self, media, state):
+        """
+        插入历史记录
+        """
+        value = {
+            "id": media.get('tmdbid'),
+            "name": media.get('title'),
+            "year": media.get('year'),
+            "type": media.get('media_type'),
+            "rating": media.get('vote')[0] if media.get('vote') else None,
+            "image": media.get('image'),
+            "state": state,
+            "add_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        if self.get_history(key=media.get('tmdbid')):
+            self.update_history(key=media.get('tmdbid'), value=value)
+        else:
+            self.history(key=media.get('tmdbid'), value=value)
+
     def stop_service(self):
         """
         停止服务
@@ -296,6 +425,7 @@ class DoubanRank(_IPluginModule):
                                                                  season=media_info.get_season_seq())
                     if item_id:
                         self.info(f"媒体服务器已存在：{media_info.get_title_string()}")
+                        self.__update_history(media=media_info, state="DOWNLOADED")
                         continue
                     # 检查是否已订阅过
                     if self.subscribe.check_history(
@@ -305,6 +435,7 @@ class DoubanRank(_IPluginModule):
                             season=media_info.get_season_string()):
                         self.info(
                             f"{media_info.get_title_string()}{media_info.get_season_string()} 已订阅过")
+                        self.__update_history(media=media_info, state="RSS")
                         continue
                     # 添加处理历史
                     self.rsshelper.simple_insert_rss_torrents(title=unique_flag, enclosure=None)
@@ -319,8 +450,12 @@ class DoubanRank(_IPluginModule):
                     )
                     if not rss_media or code != 0:
                         self.warn("%s 添加订阅失败：%s" % (media_info.get_title_string(), msg))
+                        # 订阅已存在
+                        if code == 9:
+                            self.__update_history(media=media_info, state="RSS")
                     else:
                         self.info("%s 添加订阅成功" % media_info.get_title_string())
+                        self.__update_history(media=media_info, state="RSS")
             except Exception as e:
                 self.error(str(e))
         self.info(f"所有RSS刷新完成")
